5f11f39f2f83753f67891218ffa6bb9850da0bc8
huangzhuoran <huangzhuoran@haomo-studio.com>
2024-12-17 10:26:56 +0800

    fix: 修改依赖

--- config-overrides.js
+++ config-overrides.js
@@ -29,8 +29,6 @@ module.exports = override(
   removeModuleScopePlugin(),
   babelInclude(
     [
-      path.resolve(__dirname, "node_modules/@rneui/base"),
-      path.resolve(__dirname, "node_modules/@rneui/themed"),
       path.resolve(__dirname, "node_modules/react-native-vector-icons"),
       path.resolve(__dirname, "node_modules/react-native-ratings"),
       path.resolve(__dirname, "src"),
